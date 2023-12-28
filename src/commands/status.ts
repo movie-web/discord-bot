261dfb8b53712598203735eb87b9ecefb3aed648
wadder12 <apwade75009@gmail.com>
2023-12-27 22:07:08 -0600

    first update to the ugly embed - more to come (let me do the bot PLS)

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,54 +1,71 @@
 import { Command } from '@sapphire/framework';
-import { CommandInteraction } from 'discord.js';
-
+import { CommandInteraction, EmbedBuilder } from 'discord.js';
 import { config, mwUrls } from '#src/config';
 import { isRealError } from '#src/util';
 
 export class StatusCommand extends Command {
-  public override async chatInputRun(interaction: CommandInteraction) {
+  public override async chatInputRun(interaction: CommandInteraction): Promise<void> {
     try {
       await interaction.deferReply();
+      const statusResults = await this.checkUrlsStatus(mwUrls);
 
-      const description = await Promise.all(
-        mwUrls.map(async (url) => {
-          const { ok } = await fetch(url).catch(() => ({ ok: false }));
-          return `**${url}** ${ok ? `🟢 UP` : '🔴 DOWN'}`;
-        }),
-      ).then((lines) => lines.join('\n'));
+      const embeds = this.createEmbeds(statusResults);
+      // Uncomment the next line if you use components
+      // const components = this.createComponents();
 
-      const embeds = [
-        {
-          title: 'mw status',
-          description,
-          thumbnail: {
-            url: this.container.client.user?.displayAvatarURL() ?? config.mwIconUrl,
-          },
-          color: 0xa87fd1,
-        },
-      ];
-
-      //   const components = [
-      //     new ActionRowBuilder<ButtonBuilder>().addComponents(
-      //       new ButtonBuilder()
-      //         .setLabel('submit your mirror')
-      //         .setStyle(ButtonStyle.Link)
-      //         .setURL('https://github.com/movie-web/discord-bot/edit/dev/src/mw-urls.txt'),
-      //     ),
-      //   ];
-
-      await interaction.editReply({ embeds });
+      await interaction.editReply({ embeds /*, components*/ });
     } catch (ex) {
       if (isRealError(ex as Error)) {
-        throw ex;
+        console.error('Error in StatusCommand:', ex);
+        await interaction.followUp('An error occurred while processing your request.');
       }
     }
   }
 
-  public override registerApplicationCommands(registry: Command.Registry) {
+  private async checkUrlsStatus(urls: string[]): Promise<string[]> {
+    return Promise.all(urls.map(async (url) => {
+      try {
+        const response = await fetch(url);
+        return `${response.ok ? `🟢 UP` : '🔴 DOWN'} - [${url}](${url})`;
+      } catch (error) {
+        console.error('Error checking URL:', url, error);
+        return `🔴 DOWN (Error) - [${url}](${url})`;
+      }
+    }));
+  }
+
+  private createEmbeds(statusResults: string[]): EmbedBuilder[] {
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0x0099FF)
+      .setTitle('Movie-Web Status')
+      .setURL('https://movie-web.app') // Replace with your website or relevant link
+      .setAuthor({ 
+        name: 'Movie-Web Bot', 
+        iconURL: 'https://avatars.githubusercontent.com/u/121455091?s=200&v=4', // Replace with your bot's icon URL
+        url: 'https://movie-web.app' // Replace with your website or relevant link
+      })
+      .setDescription('Current status of Movie-Web URLs')
+      .setThumbnail(this.container.client.user?.displayAvatarURL() ?? config.mwIconUrl)
+      .addFields(statusResults.map(status => ({ name: '\u200B', value: status })))
+      .setTimestamp()
+      .setFooter({ 
+        text: 'Status checked at', 
+        iconURL: 'https://avatars.githubusercontent.com/u/121455091?s=200&v=4' // Optional: Footer icon URL
+      });
+
+    return [exampleEmbed];
+  }
+
+  // Uncomment to use components
+  // private createComponents(): object[] {
+  //   // Add your component creation logic here
+  // }
+
+  public override registerApplicationCommands(registry: Command.Registry): void {
     registry.registerChatInputCommand((builder) =>
-      builder //
+      builder
         .setName('status')
-        .setDescription('movie-web status'),
+        .setDescription('Check the status of Movie-Web sites'),
     );
   }
 }
